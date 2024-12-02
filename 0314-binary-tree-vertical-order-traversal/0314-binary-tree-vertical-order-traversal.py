d9f700efe3b26c1bcc307e53142e122e56e536cf
Jabir Hussain <jabir-hussain@hotmail.co.uk>
2024-12-02 15:51:23 +0000

    Time: 3 ms (19.53%), Space: 17.3 MB (5.79%) - LeetHub

--- 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.py
+++ 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.py
@@ -4,20 +4,22 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-from collections import defaultdict
 class Solution:
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if not root:
-            return []
 
         table = defaultdict(list)
+        if not root:
+            return []
         queue = deque([(root, 0)])
+        mincol, maxcol = 0, 0
+
         while queue:
             node, col = queue.popleft()
-
             if node is not None:
                 table[col].append(node.val)
+                mincol = min(mincol, col)
+                maxcol = max(maxcol, col)
                 queue.append((node.left, col - 1))
                 queue.append((node.right, col + 1))
 
-        return [table[x] for x in sorted(table.keys())]
+        return [table[x] for x in range(mincol, maxcol + 1)]
